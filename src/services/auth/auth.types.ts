2b07ac0eb3c2a35f8ff555bd1a719c7725500f09
Dmitry Shamko <proffery@gmail.com>
2024-08-25 16:44:27 +0300

    try for Deploy

--- src/services/auth/auth.types.ts
+++ src/services/auth/auth.types.ts
@@ -1,7 +1,7 @@
-import { Models } from 'appwrite'
+import { User } from '@/services/users/users.types'
 
 export type ApiResponse = { message?: string }
-export type MeResponse = { user?: Models.User<Models.Preferences> }
+export type MeResponse = { user?: User }
 export type LoginEmailRequest = { email: string; password: string }
 export type RegistrationRequest = { email: string; name: string; password: string }
 export type VerifyEmailRequest = { secret: string; userId: string }
--- src/services/users/users.types.ts
+++ src/services/users/users.types.ts
@@ -1,7 +1,39 @@
-import { Models } from 'appwrite'
-
-type User = Models.User<Models.Preferences>
+export type User = {
+  $createdAt: string
+  $id: string
+  $updatedAt: string
+  accessedAt: string
+  email: string
+  emailVerification: boolean
+  hash: string
+  hashOptions: {
+    memoryCost: number
+    threads: number
+    timeCost: number
+    type: string
+  }
+  labels: string[]
+  mfa: boolean
+  name: string
+  password: string
+  passwordUpdate: string
+  phone: string
+  phoneVerification: boolean
+  prefs: any
+  registration: string
+  status: boolean
+  targets: {
+    $createdAt: string
+    $id: string
+    $updatedAt: string
+    identifier: string
+    name: string
+    providerId: string
+    providerType: string
+    userId: string
+  }[]
+}
 
 export type GetUserResponse = { user?: User }
 export type GetUserRequest = { userId: string }
-export type GetUsersListResponse = { total: number; users: Array<User> }
+export type GetUsersListResponse = { total: number; users: User[] }
