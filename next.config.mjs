f28d8454f15ebf835d0ea4a43910c83fe115f5d9
Dmitry Shamko <proffery@gmail.com>
2024-09-03 17:53:44 +0300

    headers changed

--- next.config.mjs
+++ next.config.mjs
@@ -4,28 +4,16 @@ const nextConfig = {
         return [
             {
                 headers: [
-                    {
-                        key: 'Access-Control-Allow-Credentials',
-                        value: 'true',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: `${process.env.NEXT_PUBLIC_HOST_BASE}`,
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value:
-
-                            'X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version',
-                    },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_HOST_BASE, },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
                 ],
-            },
-        ];
+                source: "/api/(.*)"
+            }
+        ]
     },
+
     trailingSlash: true,
 };
 
