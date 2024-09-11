2f38920d5d597ac94adca097d79d1ea0b721cce7
Dmitry Shamko <proffery@gmail.com>
2024-09-11 17:19:11 +0300

    return all back problem in alloyed domains

--- src/server/functions/oauth/sign-up-with-github.ts
+++ src/server/functions/oauth/sign-up-with-github.ts
@@ -1,4 +1,6 @@
 'use server'
+import { endpoints } from '@/common/constants/endpoints'
+import { routes } from '@/common/constants/routes'
 import { createAuthClient } from '@/server/auth-config'
 import { redirect } from 'next/navigation'
 import { OAuthProvider } from 'node-appwrite'
@@ -8,8 +10,8 @@ export async function signUpWithGithub() {
 
   const redirectUrl = await authInstance.createOAuth2Token(
     OAuthProvider.Github,
-    'https://shamko-blog.vercel.app/api/auth/create-oauth-session',
-    'https://shamko-blog.vercel.app/registration'
+    `${endpoints._base}${endpoints.auth_create_oauth_session}`,
+    `${endpoints._base}${routes.registration}`
   )
 
   return redirect(redirectUrl)
